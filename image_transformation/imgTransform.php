7ff65a3dc3f31ce24393767dd442b1baa20c3379
Mercury <mercury@archturia.systems@gmail.com>
2014-06-25 07:27:45 -0400

    An image transformation algorithm in PHP that lightens or darkens a PPM image.

---
+++ image_transformation/imgTransform.php
@@ -0,0 +1,52 @@
+<?php
+// Perform linear transformation to manipulate image grey scales by 20 pixels
+function makeImgLighter($lines){
+	$dataArray = array();
+
+	try{
+		for($i = 0; $i < count($lines); $i++){
+			//Read file lines by row
+			$lineArray = explode(" ", $lines[$i]);
+
+			for($j = 0; $j < count($lineArray); $j++){	
+			//Read line rows by column	
+				if(is_numeric($lineArray[$j])){			
+					if($lineArray[$j] >= 256){
+						$lineArray[$j] = 256;
+					}else{
+						//y=mx + b
+						$lineArray[$j] = $lineArray[$j] + 20;
+					}
+					array_push($dataArray, $lineArray[$j]);
+				}			
+			}
+		}
+
+		return $dataArray; 
+	} catch(Exception $e){
+		error_log("Linear Transformation failed.");
+	}
+}
+
+$oldfile = "sample_images/lena256_PPM.ppm";
+$lines = file($oldfile);
+$dataArray = makeImgLighter($lines);
+
+$dataString = implode(" ", $dataArray);
+$dataString = "P3\n".$dataString;
+//echo $dataString;
+$newfile = "sample_images/newlightImage.ppm";
+
+if (!$handle = fopen($newfile, 'w')) {
+     echo "Cannot open file ($filename)";
+     exit;
+}
+
+if (fwrite($handle, $dataString) === FALSE) {
+    echo "Cannot write to file ($filename)";
+    exit;
+}
+
+echo "Success, wrote data to file ($filename)";
+fclose($handle);
+?>
